e5388bbb086a2309085d9ae030a03fdf25026e80
Jura <jurijus.st@gmail.com>
2020-04-01 19:08:32 +0300

    Implemented email validation

---
+++ script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('email-form');
+const email = document.getElementById('email');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const emailValue = email.value;
+    if (!validEmail(emailValue)) {
+        form.classList.add('error');
+    } else {
+        form.classList.remove('error');
+    }
+});
+
+function validEmail(email){
+    var regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+    return regex.test(String(email).toLocaleLowerCase());
+}
